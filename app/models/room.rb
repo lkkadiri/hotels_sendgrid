2be2839dbd32913a36f5f995c4ce3dcb4882a265
Rasman <lkkadiri@gmail.com>
2013-02-13 22:41:40 -0700

    Updated the minimum on bedrooms

--- app/models/room.rb
+++ app/models/room.rb
@@ -1,8 +1,8 @@
 class Room < ActiveRecord::Base
   belongs_to :hotel
   attr_accessible :bedrooms, :beds, :booking_status, :fridge, :microwave, :name, :nightly_rate, :sauna, :smoke_free, :television, :hotel_id
-  validates :bedrooms, :numericality => { :greater_than => 1 }
-  validates :beds, :numericality => { :greater_than => 1 }
+  validates :bedrooms, :numericality => { :greater_than => 0 }
+  validates :beds, :numericality => { :greater_than => 0 }
   
   
   def self.show_available(hotel_id)
