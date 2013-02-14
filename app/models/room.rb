54fa220c59a2a840b528a73265c2a55186e24217
Rasman <lkkadiri@gmail.com>
2013-02-14 00:05:57 -0700

    Removed the User from reservation show

--- app/models/room.rb
+++ app/models/room.rb
@@ -1,5 +1,6 @@
 class Room < ActiveRecord::Base
   belongs_to :hotel
+  has_many :reservation
   attr_accessible :bedrooms, :beds, :booking_status, :fridge, :microwave, :name, :nightly_rate, :sauna, :smoke_free, :television, :hotel_id
   validates :bedrooms, :numericality => { :greater_than => 0 }
   validates :beds, :numericality => { :greater_than => 0 }
