fa3e6b62df44f1955bc8983a288d7ec439f8deef
Rasman <lkkadiri@gmail.com>
2013-02-16 16:37:51 -0700

    Modified the query to reject instead of find and then search

--- app/validators/availability.rb
+++ app/validators/availability.rb
@@ -1,8 +1,8 @@
 class Availability < ActiveModel::Validator
   def validate(record)
     rooms = Room.where("rooms.id NOT IN (SELECT reservations.room_id from reservations WHERE NOT( reservations.check_out <= ? OR reservations.check_in >= ?) )",record.check_in,record.check_out)
-    room = rooms.find { |room| room.id == record.room_id }
-    if room.nil?
+    room = rooms.reject { |room| room.id == record.room_id }
+    if room
       record.errors[:base] << 'The date you requested is taken, You can either check for other dates or check for other rooms maybe available. We Apologize for the inconvinience.'
       #send email
     end
