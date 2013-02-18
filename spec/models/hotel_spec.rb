f2ffa49f04d8e693b27864a38f308d5914958d7d
Rasman <lkkadiri@gmail.com>
2013-02-17 19:26:32 -0700

    Added the email and website validations on Hotel model, closes#5

--- spec/models/hotel_spec.rb
+++ spec/models/hotel_spec.rb
@@ -13,18 +13,17 @@ describe Hotel do
     it "#creates a hotel" do
       hotel.should_not be_nil
     end
-    it "should not let users enter invalid email" do
+    it "should not let users enter invalid email during a hotel creation" do
         lambda do
           @hotel = FactoryGirl.create(:hotel, @attr.merge(:email => 'blah'))
           @hotel.should_not be_valid
         end.should raise_error(ActiveRecord::RecordInvalid)
     end
-    it "should not let users enter invalid website" do
+    it "should not let users enter invalid website during a hotel creation" do
         lambda do
           @hotel = FactoryGirl.create(:hotel, @attr.merge(:website => 'http://send'))
           @hotel.should_not be_valid
         end.should raise_error(ActiveRecord::RecordInvalid)
     end
-    
   end  
 end
