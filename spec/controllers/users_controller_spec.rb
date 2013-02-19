6b89b24cd6b2d94d0946d438c1f5c38d3ae3c340
Rasman <lkkadiri@gmail.com>
2013-02-18 18:24:12 -0700

    Added the admin login test

---
+++ spec/controllers/users_controller_spec.rb
@@ -0,0 +1,17 @@
+require 'spec_helper'
+describe UsersController do
+  login_admin
+
+  it "should have a current_user" do
+    # note the fact that I removed the "validate_session" parameter if this was a scaffold-generated controller
+    subject.current_user.should_not be_nil
+    p subject.current_user
+  end
+
+  it "should get index" do
+    # Note, rails 3.x scaffolding may add lines like get :index, {}, valid_session
+    # the valid_session overrides the devise login. Remove the valid_session from your specs
+    get 'index'
+    response.should be_success
+  end
+end
--- spec/support/factories/user.rb
+++ spec/support/factories/user.rb
@@ -3,11 +3,8 @@ FactoryGirl.define do
     sequence(:email)                  { |n| "user_#{n}@example.com"}
     password               "password"
     password_confirmation  "password"
+    factory :admin do
+      self.admin 1
+    end
   end
-end
-Factory.define :admin, :class => User do |f|
-  f.sequence(:email) {|n| "admin#{n}@hotelsendgrid.com"}
-  f.password "admin123"
-  f.password_confirmation "admin123"
-  f.admin 1
 end
